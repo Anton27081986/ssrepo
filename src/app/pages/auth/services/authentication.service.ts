2663ceaf34707fd52f529d8af83d83532bd755a5
golubovich.av <golubovich.av@ssnab.ru>
2023-11-14 09:02:43 +0300

    feat: auth

--- src/app/pages/auth/services/authentication.service.ts
+++ src/app/pages/auth/services/authentication.service.ts
@@ -41,7 +41,7 @@ export class AuthenticationService {
     // Basic Auth
     loginBasic(Username: string, Password: string): Observable<any> {
         return this.http
-            .post<any>(`https://erp-dev.ssnab.it/auth/sign-in`, {Username, Password}, {})
+            .post<any>(`https://ssnab.it/login`, {Username, Password}, {})
             .pipe(
                 take(1),
                 takeLast(1),
