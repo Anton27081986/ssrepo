a78d9def944a9f5fb7220286dba1a909ad374419
golubovich.av <golubovich.av@ssnab.ru>
2023-11-16 12:06:23 +0300

    feat: auth

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -51,7 +51,7 @@ export class SignInComponent implements OnInit {
 
     onSubmit() {
         this.authenticationService
-            .loginBasic('nekrasov_va', 'jN*Bgf9.*f#&FBr', '/index')
+            .loginBasic('nekrasov_va', 'jN*Bgf9.*f#&FBr', 'https://erp-dev.ssnab.it/')
             .pipe(tap(val => console.log('val', val)))
             .subscribe(
                 (data: any) => {
