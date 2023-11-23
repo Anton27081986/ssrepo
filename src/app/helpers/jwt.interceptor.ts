f1921f2a19ded77a8f62f82dec19cc890e5597d1
Aleksey <golubovich.av@ssnab.ru>
2023-11-23 15:13:42 +0300

    fix: auth

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -4,7 +4,6 @@ import {Observable} from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Intercepting Request withCredentials=true ');
         request = request.clone({
