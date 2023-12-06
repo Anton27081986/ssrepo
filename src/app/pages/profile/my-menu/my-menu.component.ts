336cbe5ca82620c869eabb79dbe6ad1cf2b6ba0e
Aleksey <aleksey@ssnab>
2023-12-06 09:24:49 +0300

    fix: profile

---
+++ src/app/pages/profile/my-menu/my-menu.component.ts
@@ -0,0 +1,39 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NzIconService} from 'ng-zorro-antd/icon';
+import {AppIcons} from '@app/common/icons';
+
+@Component({
+    selector: 'app-my-menu',
+    templateUrl: './my-menu.component.html',
+    styleUrls: ['./my-menu.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MyMenuComponent {
+    myMenuForm!: FormGroup;
+
+    constructor(
+        private readonly iconService: NzIconService,
+        private readonly formBuilder: FormBuilder,
+    ) {
+        this.iconService.addIconLiteral('ss:search', AppIcons.iconSearch);
+        this.iconService.addIconLiteral('ss:favorite', AppIcons.star);
+        this.iconService.addIconLiteral('ss:closeLight', AppIcons.iconCloseLight);
+        this.iconService.addIconLiteral('ss:arrowRight', AppIcons.arrowRight);
+    }
+
+    ngOnInit() {
+        this.myMenuForm = this.formBuilder.group({
+            login: [
+                '',
+                [
+                    Validators.required,
+                    // Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+                ],
+            ],
+            password: ['', Validators.required],
+        });
+    }
+
+    onSubmit() {}
+}
---
+++ src/app/pages/profile/settings/settings.component.ts
@@ -0,0 +1,29 @@
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SettingsComponent implements OnInit {
+    settingsForm!: FormGroup;
+
+    constructor(private readonly formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.settingsForm = this.formBuilder.group({
+            login: [
+                '',
+                [
+                    Validators.required,
+                    // Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+                ],
+            ],
+            password: ['', Validators.required],
+        });
+    }
+
+    onSubmit() {}
+}
