253277f3148897ead0b63b8de57753d76516d60a
Aleksey <aleksey@ssnab>
2023-12-15 09:56:14 +0300

    feat: спасибо

---
+++ src/app/components/thank-colleague/thank-colleague.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thank-colleague',
+  templateUrl: './thank-colleague.component.html',
+  styleUrls: ['./thank-colleague.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ThankColleagueComponent {
+
+}
