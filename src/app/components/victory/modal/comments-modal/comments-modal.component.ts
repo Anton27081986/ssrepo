797f057a7c32da864d8b6f9cac7f1fcb2406dfff
Aleksey <golubovich.av@ssnab.ru>
2024-01-15 11:54:02 +0300

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit cb457f349e29c78a26f9641ada636ffaff01bf78, reversing
    changes made to 05103500812a407de3ffa31b28ce05d55963abca.

--- src/app/components/victory/modal/comments-modal/comments-modal.component.ts
+++ src/app/components/victory/modal/comments-modal/comments-modal.component.ts
@@ -1,4 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NZ_MODAL_DATA, NzModalRef} from 'ng-zorro-antd/modal';
+import {Observable} from 'rxjs';
+import {ApiService} from '@app/shared/services/api/api.service';
 
 @Component({
     selector: 'app-comments-modal',
@@ -6,4 +10,49 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
     styleUrls: ['./comments-modal.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommentsModalComponent {}
+export class CommentsModalComponent {
+    readonly nzModalData: any = inject(NZ_MODAL_DATA);
+    userData!: Observable<any>;
+
+    loginForm!: FormGroup;
+    submitted = false;
+    loading = false;
+    isConfirmLoading = false;
+
+    peoplelikesOpen = false;
+    // attachIsVisible = false;
+    commentVisible = false;
+    attachVisible = false;
+
+    constructor(
+        private readonly apiService: ApiService,
+        private readonly modal: NzModalRef,
+        private readonly formBuilder: FormBuilder,
+    ) {}
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            login: [
+                '',
+                [
+                    Validators.required,
+                    // Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+                ],
+            ],
+            password: ['', Validators.required],
+        });
+    }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+    }
+
+    search() {}
+}
