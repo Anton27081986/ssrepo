f96bbeeded73724f8930af0775045dd15af1fd67
Aleksey <golubovich.av@ssnab.ru>
2024-01-21 22:48:10 +0300

    fix: api-service

--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable, throwError} from 'rxjs';
+import {Observable} from 'rxjs';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {IMainMenu} from '@app/components/main-menu/main-menu.interface';
 import {environment} from '@environments/environment.development';
@@ -149,9 +149,5 @@ export class ApiService {
                 .set('Limit', Limit)
                 .set('Offset', Offset),
         });
-    }
-
-    private formatErrors(error: any) {
-        return throwError(error.error);
     }
 }
