be96074d6ac55f80396e22e0ca23ec0fc89a3494
Aleksey <aleksey@ssnab>
2024-01-30 15:13:09 +0300

    fix: настройка prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://erp.ssnab.it',
+    apiUrl: 'https://erp.ssnab.itdev',
     locales: ['en', 'ru'],
     defaultLocale: 'ru',
     applicationTitle: 'ERP SSNAB',
