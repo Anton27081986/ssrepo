b307413259c3b68ec25d82054ef20ce34dcdfd48
Aleksey <aleksey@ssnab>
2024-03-07 20:48:52 +0300

    fix: админский вход

--- src/app/components/modal/modal.module.ts
+++ src/app/components/modal/modal.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ModalComponent } from '@app/components/modal/modal.component';
+import { ResultItemComponent } from '@app/shared/components/search/result-item/result-item.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { JsonPipe, NgClass } from '@angular/common';
 
 @NgModule({
-	declarations: [ModalComponent],
-	exports: [ModalComponent],
+	declarations: [ModalComponent, ResultItemComponent],
+	exports: [ModalComponent, ResultItemComponent],
+	imports: [NzIconModule, NgClass, JsonPipe],
 })
 export class ModalModule {}
