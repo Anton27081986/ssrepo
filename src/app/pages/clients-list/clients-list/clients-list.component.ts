3f7d692c934e569c09b688724f2b2fc2db5b2870
as.borodulin <>
2024-03-12 19:00:23 +0300

    update models from backend

--- src/app/pages/clients-list/clients-list/clients-list.component.ts
+++ src/app/pages/clients-list/clients-list/clients-list.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {UntilDestroy} from "@ngneat/until-destroy";
+import { UntilDestroy } from '@ngneat/until-destroy';
 
 @UntilDestroy()
 @Component({
