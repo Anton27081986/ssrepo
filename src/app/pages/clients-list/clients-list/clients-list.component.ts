f167569775d3479ba79bfbde45a1e136174b44b6
as.borodulin <>
2024-03-12 18:52:18 +0300

    added pages for clients-list and client-card, refactoring base layout

---
+++ src/app/pages/clients-list/clients-list/clients-list.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {UntilDestroy} from "@ngneat/until-destroy";
+
+@UntilDestroy()
+@Component({
+	selector: 'app-clients-list',
+	templateUrl: './clients-list.component.html',
+	styleUrls: ['./clients-list.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClientsListComponent {
+	public clientsMock = [
+		{
+			name: 'Alex',
+			id: 1,
+		},
+		{
+			name: 'Sergey',
+			id: 2,
+		},
+	];
+}
