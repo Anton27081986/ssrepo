70f78a96999b86ebc6ecbef3101b3066388ead6b
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2024-03-19 02:10:10 +0300

    Компонент автарки

--- src/app/pages/sandbox/sandbox.module.ts
+++ src/app/pages/sandbox/sandbox.module.ts
@@ -5,9 +5,10 @@ import { SandboxComponent } from '@app/pages/sandbox/sandbox.component';
 import { SandboxRoutingModule } from '@app/pages/sandbox/sandbox-routing.module';
 import {MailModule} from "@app/components/mail/mail.module";
 import {CardModule} from "@app/shared/components/card/card.module";
+import {AvatarModule} from "@app/shared/components/avatar/avatar.module";
 
 @NgModule({
 	declarations: [SandboxComponent],
-	imports: [SandboxRoutingModule, CommonModule, RouterLink, MailModule, CardModule],
+    imports: [SandboxRoutingModule, CommonModule, RouterLink, MailModule, CardModule, AvatarModule],
 })
 export class SandboxModule {}
---
+++ src/app/shared/components/avatar/avatar.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'ss-avatar',
+	templateUrl: './avatar.component.html',
+	styleUrls: ['./avatar.component.scss'],
+})
+export class AvatarComponent implements OnInit{
+	@Input() src!: string;
+	@Input() isSquare = true;
+	@Input() size: 'xs' | 's' | 'm' | 'l' = 's';
+
+	protected noImage = false
+
+	protected width: number = 40;
+	protected height: number = 40;
+
+	protected onImageError() {
+		this.noImage = true;
+	}
+
+	public ngOnInit() {
+		switch (this.size) {
+			case "l":
+				this.width = this.isSquare ? 80 : 72;
+				this.height = this.isSquare ? 80 : 96;
+				return
+			case "m":
+				this.width = this.isSquare ? 58 : 58;
+				this.height = this.isSquare ? 58 : 77;
+				return
+			case "s":
+				this.width = this.isSquare ? 40 : 44;
+				this.height = this.isSquare ? 40 : 59;
+				return
+			case "xs":
+				this.width = this.isSquare ? 32 : 28;
+				this.height = this.isSquare ? 32 : 36;
+				return
+		}
+	}
+}
