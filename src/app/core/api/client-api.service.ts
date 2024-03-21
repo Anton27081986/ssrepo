7b6c097a35279d8e622d6849ead8ae5c3fb8741b
as.borodulin <>
2024-03-21 21:26:42 +0300

    add api methods, update models from swagger

--- src/app/core/api/client-api.service.ts
+++ src/app/core/api/client-api.service.ts
@@ -3,6 +3,10 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment.development';
 import { IClientDto } from '@app/core/models/company/client-dto';
+import { IClientStatus } from '@app/core/models/company/client-status';
+import { IResponse } from '@app/core/utils/response';
+import { IClientItemDto } from '@app/core/models/company/client-item-dto';
+import { IDictionaryItemDto } from '@app/core/models/company/dictionary-item-dto';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,7 +14,60 @@ import { IClientDto } from '@app/core/models/company/client-dto';
 export class ClientApiService {
 	public constructor(private readonly http: HttpClient) {}
 
-	public getClients() {}
+	public getClients(
+		code?: number,
+		name?: string,
+		categoryId?: number,
+		contactorId?: number,
+		managerId?: number,
+		status?: IClientStatus,
+		withoutBaseManager?: boolean,
+		limit?: number,
+		offset?: number,
+	) {
+		const params = new HttpParams();
+
+		if (code) {
+			params.set('code', code.toString());
+		}
+
+		if (name) {
+			params.set('name', name.toString());
+		}
+
+		if (categoryId) {
+			params.set('categoryId', categoryId.toString());
+		}
+
+		if (contactorId) {
+			params.set('contactorId', contactorId.toString());
+		}
+
+		if (managerId) {
+			params.set('managerId', managerId.toString());
+		}
+
+		if (status) {
+			params.set('status', status.toString());
+		}
+
+		if (withoutBaseManager) {
+			params.set('withoutBaseManager', withoutBaseManager.toString());
+		}
+
+		if (limit) {
+			params.set('limit', limit.toString());
+		}
+
+		if (offset) {
+			params.set('offset', offset.toString());
+		}
+
+		return this.http.get<IResponse<IClientItemDto>>(
+			`${environment.apiUrl}/api/company/clients`,
+			{ params },
+		);
+	}
 
 	public getClientCardById(id: number): Observable<IClientDto> {
 		return this.http.get<IClientDto>(`${environment.apiUrl}/api/company/client/${id}`, {
@@ -26,9 +83,39 @@ export class ClientApiService {
 
 	public setMainManager() {}
 
-	public getContractors() {}
+	public getContractors(searchTerm: string) {
+		return this.http.get<IDictionaryItemDto[]>(
+			`${environment.apiUrl}/api/company/clients/contractors`,
+			{
+				params: new HttpParams().set('query', searchTerm),
+			},
+		);
+	}
 
-	public getSubSectors() {}
+	public getRegions(searchTerm: string) {
+		return this.http.get<IDictionaryItemDto[]>(
+			`${environment.apiUrl}/api/company/clients/regions`,
+			{
+				params: new HttpParams().set('query', searchTerm),
+			},
+		);
+	}
 
-	public getRegions() {}
+	public getCategories(searchTerm: string) {
+		return this.http.get<IDictionaryItemDto[]>(
+			`${environment.apiUrl}/api/company/clients/categories`,
+			{
+				params: new HttpParams().set('query', searchTerm),
+			},
+		);
+	}
+
+	public getSubSectors(searchTerm: string) {
+		return this.http.get<IDictionaryItemDto[]>(
+			`${environment.apiUrl}/api/company/clients/subsectors`,
+			{
+				params: new HttpParams().set('query', searchTerm),
+			},
+		);
+	}
 }
