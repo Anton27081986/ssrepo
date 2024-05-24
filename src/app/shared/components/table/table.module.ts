c72fd2484293c15aea237f902a36a0a2d6b8407f
Aleksey <golubovich.av@ssnab.ru>
2024-05-24 08:53:36 +0300

    Задачи из запросов

--- src/app/shared/components/table/table.module.ts
+++ src/app/shared/components/table/table.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { TableComponent } from '@app/shared/components/table/table.component';
-import { NgForOf, NgIf, SlicePipe } from '@angular/common';
+import { NgClass, NgForOf, NgIf, SlicePipe } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
 	declarations: [TableComponent],
 	exports: [TableComponent],
-	imports: [NgForOf, NzTableModule, NzToolTipModule, SlicePipe, NgIf],
+	imports: [NgForOf, NzTableModule, NzToolTipModule, SlicePipe, NgIf, NgClass],
 })
 export class TableModule {}
