7adc069e518a442ba2f95bcecf85f0cd6f028347
as.borodulin <>
2024-05-27 17:21:02 +0300

    rework bind main properties

--- src/app/widgets/history/history.module.ts
+++ src/app/widgets/history/history.module.ts
@@ -3,21 +3,22 @@ import { HistoryComponent } from '@app/widgets/history/history.component';
 import { CardModule } from '@app/shared/components/card/card.module';
 import { HeadlineModule } from '@app/shared/components/typography/headline/headline.module';
 import { IconModule } from '@app/shared/components/icon/icon.module';
-import { NgForOf, NgIf } from '@angular/common';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
 import { TextModule } from '@app/shared/components/typography/text/text.module';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 
 @NgModule({
 	declarations: [HistoryComponent],
 	exports: [HistoryComponent],
-	imports: [
-		CardModule,
-		HeadlineModule,
-		IconModule,
-		NgIf,
-		TextModule,
-		NgForOf,
-		NzPaginationModule,
-	],
+    imports: [
+        CardModule,
+        HeadlineModule,
+        IconModule,
+        NgIf,
+        TextModule,
+        NgForOf,
+        NzPaginationModule,
+        DatePipe,
+    ],
 })
 export class HistoryModule {}
