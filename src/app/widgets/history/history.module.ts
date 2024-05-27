d06b968980eb98725533c6beb3dbd6946218f838
as.borodulin <>
2024-05-27 17:39:07 +0300

    added table with changes

--- src/app/widgets/history/history.module.ts
+++ src/app/widgets/history/history.module.ts
@@ -6,19 +6,21 @@ import { IconModule } from '@app/shared/components/icon/icon.module';
 import {DatePipe, NgForOf, NgIf} from '@angular/common';
 import { TextModule } from '@app/shared/components/typography/text/text.module';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import {NzTableModule} from "ng-zorro-antd/table";
 
 @NgModule({
 	declarations: [HistoryComponent],
 	exports: [HistoryComponent],
-    imports: [
-        CardModule,
-        HeadlineModule,
-        IconModule,
-        NgIf,
-        TextModule,
-        NgForOf,
-        NzPaginationModule,
-        DatePipe,
-    ],
+	imports: [
+		CardModule,
+		HeadlineModule,
+		IconModule,
+		NgIf,
+		TextModule,
+		NgForOf,
+		NzPaginationModule,
+		DatePipe,
+		NzTableModule,
+	],
 })
 export class HistoryModule {}
