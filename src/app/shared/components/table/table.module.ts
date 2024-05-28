32b5f21cd9c3c482cf46ccc89d282f24a98016ed
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2024-05-28 10:24:31 +0300

    Фикс табов в переписке. Убран бордер активного таба

--- src/app/shared/components/table/table.module.ts
+++ src/app/shared/components/table/table.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { TableComponent } from '@app/shared/components/table/table.component';
-import {NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, SlicePipe} from '@angular/common';
+import {NgClass, NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, SlicePipe} from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
 	declarations: [TableComponent],
 	exports: [TableComponent],
-	imports: [NgForOf, NzTableModule, NzToolTipModule, SlicePipe, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
+    imports: [NgForOf, NzTableModule, NzToolTipModule, SlicePipe, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgClass],
 })
 export class TableModule {}
