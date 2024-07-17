09485f58cdc390b63bc60e4d62738bac8e134ed9
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2024-07-17 10:25:53 +0300

    Вход по дизайну. Скрыта кнопка восстановления пароля

--- src/app/shared/components/inputs/input/input.component.ts
+++ src/app/shared/components/inputs/input/input.component.ts
@@ -7,7 +7,7 @@ import { ControlValueAccessor, NgControl } from '@angular/forms';
 	styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements ControlValueAccessor {
-	@Input() public size: 'large' | 'medium' = 'medium';
+	@Input() public size: 'large' | 'medium' | 'small' = 'medium';
 	@Input() public disabled: boolean = false;
 	@Input() public label: string | undefined;
 	@Input() public placeholder: string = '';
