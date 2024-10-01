28677a4652fb1face83e0560e870eb831bb82583
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2024-10-01 13:43:20 +0300

    reg environment

---
+++ src/environments/environment.reg.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://erp-reg.ssnab.it',
+    locales: ['en', 'ru'],
+    defaultLocale: 'ru',
+    applicationTitle: 'ERP SSNAB',
+	tabTitle: 'REG',
+	name: 'reg',
+	redirectUrl: 'https://ssnab.it/index',
+};
