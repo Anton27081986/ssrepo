a9fccab8d38af284dc63c845a5c389ad969b4890
Ващенко Геннадий Викторович <vashchenko.gv@ssnab.ru>
2024-10-23 13:09:58 +0000

    Feature/2054 new

--- src/app/core/utils/date.ts
+++ src/app/core/utils/date.ts
@@ -13,3 +13,10 @@ export function dateTimeFromIsoString(dateString: string | undefined): string {
 
 	return `${day}:${month}:${year} ${hours}:${minutes}`;
 }
+
+export function getTime(date: Date): string {
+	const hours = `0${date.getHours()}`.slice(-2);
+	const minutes = `0${date.getMinutes()}`.slice(-2);
+
+	return `${hours}:${minutes}`;
+}
---
+++ src/app/shared/components/inputs/date-time-picker/date-time-picker.imports.ts
@@ -0,0 +1,18 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MaskitoDirective } from '@maskito/angular';
+import { IconModule } from '@app/shared/components/icon/icon.module';
+
+export const DateTimePickerImports = [
+	FormsModule,
+	ReactiveFormsModule,
+	MatDatepickerModule,
+	MatFormFieldModule,
+	MatIconModule,
+	MatInputModule,
+	MaskitoDirective,
+	IconModule,
+];
