7e8dc95ccfd4687f38a44437d81d2d9dd8be1f5c
Gennady Vaschenko <gennadyv@ideo-digital.com>
2025-01-13 14:42:24 +0300

    feature/2819: separate queries for checking permissions of different modules

--- src/app/core/guards/procurements-permissions.guard.ts
+++ src/app/core/guards/procurements-permissions.guard.ts
@@ -1,28 +1,26 @@
-import { CanActivate, Router } from '@angular/router';
-import { Injectable } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 import { Permissions } from '@app/core/constants/permissions.constants';
-import { map, Observable } from 'rxjs';
 import { PermissionsFacadeService } from '@app/core/facades/permissions-facade.service';
+import { map } from 'rxjs';
+import { ModulesWithPermissionsEnum } from '@app/core/models/modules-with-permissions';
 
-@Injectable({ providedIn: 'root' })
-export class ProcurementsPermissionsGuard implements CanActivate {
-	public constructor(
-		private readonly router: Router,
-		private readonly permissionsFacadeService: PermissionsFacadeService,
-	) {}
+export const procurementsPermissionsGuard: CanActivateFn = () => {
+	const router = inject(Router);
+	const permissionsFacadeService = inject(PermissionsFacadeService);
 
-	public canActivate(): Observable<boolean> {
-		return this.permissionsFacadeService.procurementsPermissions$.pipe(
-			map(permissions => {
-				const checkPermission = permissions.items.find(
-					item => item === Permissions.CLIENT_PROCUREMENTS_URL_READ,
-				);
-
-				if (checkPermission) {
+	return permissionsFacadeService
+		.checkModulePermissions(
+			ModulesWithPermissionsEnum.Procurements,
+			Permissions.CLIENT_PROCUREMENTS_URL_READ,
+		)
+		.pipe(
+			map(isAllowAccess => {
+				if (isAllowAccess) {
 					return true;
 				}
 
-				this.router.navigate(['not-permission'], {
+				router.navigate(['not-permission'], {
 					queryParams: {
 						redirectUrl: 'raw-material-accounting',
 					},
@@ -31,5 +29,4 @@ export class ProcurementsPermissionsGuard implements CanActivate {
 				return false;
 			}),
 		);
-	}
-}
+};
