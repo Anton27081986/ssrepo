49e97889243779f0211f9543def1891771de8e2e
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2025-03-31 15:40:50 +0300

    Поле "плательщик" должно быть нередактируемым

--- src/app/core/models/completed-work-acts/update-act.ts
+++ src/app/core/models/completed-work-acts/update-act.ts
@@ -1,12 +1,7 @@
 export interface IUpdateAct {
-	externalActNumber?: string | null;
-	externalActDate?: string | null;
-	internalActNumber?: string | null;
-	internalActDate?: string | null;
 	dateUpload?: string | null;
 	applicantUserId?: number | null;
 	buUnitId?: number | null;
-	payerContractorId?: number | null;
 	payerBuUnitId?: number | null;
 	providerContractorId?: number | null;
 	inn?: string | null;
