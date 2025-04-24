d3b90d4a1e85d3db16a7a0ecc3c9006c890e9ebc
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-04-24 12:53:41 +0300

    feature/dev3: fix header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,52 +1,78 @@
-import { ChangeDetectionStrategy, Component, OnInit, Signal } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	OnInit,
+	Signal,
+} from '@angular/core';
 import { AppRoutes } from '@app/common/routes';
 import { Observable } from 'rxjs';
 import { IMenuItemDto } from '@app/core/models/company/menu-item-dto';
 import { IUserProfile } from '@app/core/models/user-profile';
 import { MainMenuFacadeService } from '@app/core/facades/main-menu-facade.service';
-import {Router, RouterLink} from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UserProfileStoreService } from '@app/core/states/user-profile-store.service';
 import { IDictionaryItemDto } from '@app/core/models/company/dictionary-item-dto';
-import { TooltipPosition, TooltipTheme } from '@app/shared/components/tooltip/tooltip.enums';
-import { ButtonType, IconPosition, IconType, Size } from '@front-components/components';
+import {
+	TooltipPosition,
+	TooltipTheme,
+} from '@app/shared/components/tooltip/tooltip.enums';
+import {
+	ButtonType,
+	IconPosition,
+	IconType,
+	Size,
+} from '@front-components/components';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ChatBotFacadeService } from '@app/core/facades/chat-bot-facade.service';
-import {IconComponent} from "@app/shared/components/icon/icon.component";
-import {MainMenuComponent} from "@app/shared/components/main-menu/main-menu.component";
-import {AsyncPipe} from "@angular/common";
-import {SearchInputComponent} from "@app/shared/components/inputs/search-input/search-input.component";
-import {NotificationComponent} from "@app/shared/components/notification/notification.component";
-import {ButtonComponent} from "@app/shared/components/buttons/button/button.component";
-import {ChatBotComponent} from "@app/widgets/chat-bot/chat-bot.component";
-import {DropdownButtonComponent} from "@app/shared/components/buttons/dropdown-button/dropdown-button.component";
-import {ProfilePopupComponent} from "@app/shared/components/profile-popup/profile-popup.component";
+import { IconComponent } from '@app/shared/components/icon/icon.component';
+import { MainMenuComponent } from '@app/shared/components/main-menu/main-menu.component';
+import { SearchInputComponent } from '@app/shared/components/inputs/search-input/search-input.component';
+import { NotificationComponent } from '@app/shared/components/notification/notification.component';
+import { ButtonComponent } from '@app/shared/components/buttons/button/button.component';
+import { ChatBotComponent } from '@app/widgets/chat-bot/chat-bot.component';
+import { DropdownButtonComponent } from '@app/shared/components/buttons/dropdown-button/dropdown-button.component';
+import { ProfilePopupComponent } from '@app/shared/components/profile-popup/profile-popup.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	standalone: true,
 	imports: [
 		IconComponent,
-		MainMenuComponent,
-		AsyncPipe,
 		RouterLink,
+		MainMenuComponent,
 		SearchInputComponent,
 		NotificationComponent,
 		ButtonComponent,
 		ChatBotComponent,
 		DropdownButtonComponent,
-		ProfilePopupComponent
+		ProfilePopupComponent,
+		AsyncPipe,
 	],
-	standalone: true
 })
 export class HeaderComponent implements OnInit {
 	public listMenu$?: Observable<IMenuItemDto[] | null>;
 	public userProfile$?: Observable<IUserProfile | null>;
 	public profilePopup$?: Observable<boolean | null>;
 
+	public aiPermission: Signal<boolean> = toSignal(
+		this.mainMenuFacade.aiPermission$,
+		{
+			initialValue: false,
+		},
+	);
+
 	protected readonly AppRoutes = AppRoutes;
 	public route: string | undefined;
-	public constructor(
+	protected readonly TooltipPosition = TooltipPosition;
+	protected readonly TooltipTheme = TooltipTheme;
+	protected readonly ButtonType = ButtonType;
+	protected readonly Size = Size;
+	protected readonly IconType = IconType;
+	constructor(
 		private readonly mainMenuFacade: MainMenuFacadeService,
 		private readonly userStateService: UserProfileStoreService,
 		public readonly _router: Router,
@@ -70,14 +96,8 @@ export class HeaderComponent implements OnInit {
 		}
 	}
 
+	protected readonly IconPosition = IconPosition;
 	public openChatBot(): void {
 		this.chatBotFacade.toggleBot();
 	}
-
-	protected readonly TooltipPosition = TooltipPosition;
-	protected readonly TooltipTheme = TooltipTheme;
-	protected readonly ButtonType = ButtonType;
-	protected readonly Size = Size;
-	protected readonly IconType = IconType;
-	protected readonly IconPosition = IconPosition;
 }
