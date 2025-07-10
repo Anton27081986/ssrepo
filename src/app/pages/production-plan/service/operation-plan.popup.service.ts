e46a3f960049dec969f89b8bf45f4517502c6768
Харечко Антон Вячеславович <kharechko.av@ssnab.ru>
2025-07-10 12:23:41 +0000

    add comments popup

--- src/app/pages/production-plan/service/operation-plan.popup.service.ts
+++ src/app/pages/production-plan/service/operation-plan.popup.service.ts
@@ -13,9 +13,17 @@ import {
 	UpdateRawMaterialsData,
 } from '@app/pages/production-plan/modal/modal-update-raw-materials/modal-update-raw-materials.component';
 import {
+	CreateCommentsModalComponent,
+	CreateCommentsModalData,
+} from '@app/pages/production-plan/modal/create-comments-modal/create-comments-modal.component';
+import {
 	ApproveMaterialComponent,
 	ApproveMaterialData,
 } from '@app/pages/production-plan/modal/approve-material/approve-material.component';
+import {
+	AddCommentsModalComponent,
+	AddCommentsModalData,
+} from '@app/pages/production-plan/modal/add-comments-modal/add-comments-modal.component';
 
 @Injectable({ providedIn: 'root' })
 export class OperationPlanPopupService {
@@ -27,6 +35,24 @@ export class OperationPlanPopupService {
 			{ weekId },
 			true,
 			'920px',
+		);
+	}
+
+	public openCreateCommentsModal(id: number): ModalRef {
+		return this.popup.openModal<CreateCommentsModalData>(
+			CreateCommentsModalComponent,
+			{ id },
+			false,
+			'449px',
+		);
+	}
+
+	public openAddCommentsModal(id: number): ModalRef {
+		return this.popup.openModal<AddCommentsModalData>(
+			AddCommentsModalComponent,
+			{ id },
+			false,
+			'449px',
 		);
 	}
 
