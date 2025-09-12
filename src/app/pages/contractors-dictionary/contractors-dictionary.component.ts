03fe9f98077f06aed4497f95a798121ad9dee7d9
Gennady Vaschenko <vashchenko.gv@ssnab.ru>
2025-09-12 14:42:36 +0300

    fix/4741-contractor-card-dialog: add dialog component

--- src/app/pages/contractors-dictionary/contractors-dictionary.component.ts
+++ src/app/pages/contractors-dictionary/contractors-dictionary.component.ts
@@ -1,5 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ContractorCardDialogComponent } from '@app/pages/contractors-dictionary/contractor-card-dialog/contractor-card-dialog.component';
+import {
+	ButtonComponent,
+	ButtonType,
+	ModalRef,
+	SharedPopupService,
+} from '@front-library/components';
+import { IContractorCardSidePageData } from '@app/pages/contractors-dictionary/models/contractor-card-side-page-data';
 
 @Component({
 	selector: 'contractors-dictionary',
@@ -7,8 +15,19 @@ import {RouterOutlet} from "@angular/router";
 	styleUrls: ['./contractors-dictionary.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-    imports: [
-        RouterOutlet
-    ],
+	imports: [RouterOutlet, ButtonComponent],
 })
-export class ContractorsDictionaryComponent {}
+export class ContractorsDictionaryComponent {
+	private readonly popup = inject(SharedPopupService);
+
+	protected readonly ButtonType = ButtonType;
+
+	public contractorCardDialog(): ModalRef {
+		return this.popup.openRightSidePage<IContractorCardSidePageData>(
+			ContractorCardDialogComponent,
+			{ id: 1235 },
+			'820px',
+			false
+		);
+	}
+}
---
+++ src/app/pages/contractors-dictionary/models/contractor-card-side-page-data.ts
@@ -0,0 +1,3 @@
+export interface IContractorCardSidePageData {
+	id: number;
+}
