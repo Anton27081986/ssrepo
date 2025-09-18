07387fa072681874e4577d3fc87bf17f3a297697
Romqaa <Mr.Romqaa2015@yandex.ru>
2025-09-18 10:30:28 +0300

    feature/4509 Добавить основной фильтр на tabs

---
+++ src/app/pages/contractors-dictionary/models/contractors-dictionary-request-param.ts
@@ -0,0 +1,5 @@
+import { TabContractorsEnum } from '@app/pages/contractors-dictionary/contractors-page/contractors-page.component';
+
+export interface ContractorsDictionaryRequestParams {
+	quickFilter: TabContractorsEnum;
+}
--- src/app/pages/contractors-dictionary/services/contractors.service.ts
+++ src/app/pages/contractors-dictionary/services/contractors.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { IResponse } from '@app/core/utils/response';
 import { ContractorsApiService } from '@app/pages/contractors-dictionary/services/contractors-api.service';
 import { ContractorsItemDto } from '@app/pages/contractors-dictionary/models/contractors-item-dto';
+import { ContractorsDictionaryRequestParams } from '@app/pages/contractors-dictionary/models/contractors-dictionary-request-param';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,7 +11,9 @@ import { ContractorsItemDto } from '@app/pages/contractors-dictionary/models/con
 export class ContractorsService {
 	private apiService: ContractorsApiService = inject(ContractorsApiService);
 
-	public getContractors(): Observable<IResponse<ContractorsItemDto>> {
-		return this.apiService.getContractors();
+	public getContractors(
+		param: ContractorsDictionaryRequestParams
+	): Observable<IResponse<ContractorsItemDto>> {
+		return this.apiService.getContractors(param);
 	}
 }
